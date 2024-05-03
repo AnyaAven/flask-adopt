a93b4a1d96ed9b4afe978e047ce232c67541b40e
Jacob <jacobrashid99@gmail.com>
2024-05-03 14:58:01 -0700

    /add route

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import os
 
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, dbx, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -34,3 +35,34 @@ def home():
         "pet_listings.jinja",
         pets=pets
     )
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet_form():
+    """Pet add form"""
+    
+    form = AddPetForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        
+        pet = Pet(
+            name=name, 
+            species=species, 
+            photo_url=photo_url, 
+            age=age, 
+            notes=notes
+        )
+        
+        db.session.add(pet)
+        db.session.commit()
+        
+        flash(f"Added {pet.name} successfully!")
+        
+        return redirect("/")
+    
+    else:
+        return render_template("add_pet_form.jinja", form=form)
