d0a02306b5b0ac349ca179958f73d5e441bf5330
Jacob <jacobrashid99@gmail.com>
2024-05-03 12:32:39 -0700

    form started, WIP

--- forms.py
+++ forms.py
@@ -1,1 +1,11 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import URL
+
+class AddPetForm(FlaskForm):
+    name = StringField("Pet Name")
+    species = StringField("Pet Species")
+    photo_url = StringField("Pet Photo", validators=[URL(require_tld=False, message="Invalid Image URL!")])
+    age = StringField("Pet Age")
+    notes = TextAreaField("Pet Notes")
