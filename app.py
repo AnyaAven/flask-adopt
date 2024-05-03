1ef58305c623d3d1b8eebeea1ca487d3d012a42f
Anya Aven <leafeon96@gmail.com>
2024-05-03 15:18:24 -0400

    added home page pet listing

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, dbx
+from models import db, dbx, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -21,3 +21,16 @@ db.init_app(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def home():
+    """ Display homepage with the list of pets """
+
+    q_pets = db.select(Pet).order_by(Pet.available) # TODO: Does this order correctly?
+    pets = dbx(q_pets).scalars().all()
+
+    return render_template(
+        "pet_listings.jinja",
+        pets=pets
+    )
