e8b45beb846ad1ef488884d987cba091219fcfc1
Anya Aven <leafeon96@gmail.com>
2024-05-03 18:54:51 -0400

    Format

--- forms.py
+++ forms.py
@@ -1,33 +1,39 @@
 """Forms for adopt app."""
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, TextAreaField, SelectField
-from wtforms.validators import URL, InputRequired
+from wtforms.validators import URL, InputRequired, Optional
+
 
 class AddPetForm(FlaskForm):
     """Form for adding new pets"""
-    
+
     name = StringField("Pet Name", validators=[InputRequired()])
-    
+
     species = SelectField(
-        "Pet Species", 
-        choices=[('cat', 'Cat'), 
-                 ('dog', 'Dog'), 
-                 ('porcupine', 'Porcupine')], 
+        "Pet Species",
+        choices=[('cat', 'Cat'),
+                 ('dog', 'Dog'),
+                 ('porcupine', 'Porcupine')],
         validators=[InputRequired()]
     )
-    
+
     photo_url = StringField(
-        "Pet Photo", 
-        validators=[URL(require_tld=False, message="Invalid Image URL!")]
+        "Pet Photo",
+        validators=[
+            URL(
+                require_tld=False,
+                message="Invalid Image URL!"),
+            Optional()
+        ],
     )
-    
+
     age = SelectField(
-        "Pet Age", 
-        choices=[('baby', 'Baby'), 
-                ('young', 'Young'), 
-                ('adult', 'Adult'), 
-                ('senior', 'Senior')], 
+        "Pet Age",
+        choices=[('baby', 'Baby'),
+                 ('young', 'Young'),
+                 ('adult', 'Adult'),
+                 ('senior', 'Senior')],
         validators=[InputRequired()]
     )
-    
+
     notes = TextAreaField("Pet Notes")
