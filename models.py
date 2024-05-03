19ca6c576704d4c73cb9b3eaa1a866b1f0b3388d
Anya Aven <leafeon96@gmail.com>
2024-05-03 15:18:33 -0400

    Finished Pet model

--- models.py
+++ models.py
@@ -4,3 +4,51 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 dbx = db.session.execute
+
+class Pet(db.model):
+    """ Pet class """
+
+    __table__ = "pets"
+
+    id = db.mapped_column(
+        db.Integer,
+        db.Indentity(),
+        primary_key=True
+    )
+
+    name = db.mapped_column(
+        db.String(50),
+        nullable=False
+    )
+
+    species = db.mapped_column(
+        db.String(100),
+        nullable=False
+    )
+
+    photo_url = db.mapped_column(
+        db.String(2048),
+        default='',
+        nullable=False
+    )
+
+    age = db.mapped_column(
+        db.String(50),
+        db.CheckConstraint("age IN" + "('baby', 'young', 'adult', 'senior')"),
+        nullable=False
+    )
+
+    notes = db.mapped_column(
+        db.Text,
+        default='',
+        nullable=False
+        )
+
+    available = db.mapped_colum(
+        db.Boolean,
+        default=True,
+        nullable=False
+    )
+
+
+
